99fe21146493fde42b283e6314bcc2e258c6272e
vss-py <victorsantos.py@gmail.com>
2019-03-31 12:12:12 -0300

    debugtrue

--- unesp_apps/settings.py
+++ unesp_apps/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ny&%jt(%hrle4uvr$yf=m$cxr-4d4n!on^j769dz3h0yc)-ida'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.localhost', '.herokuapp.com']
+#ALLOWED_HOSTS = ['127.0.0.1', '.localhost', '.herokuapp.com']
 
 
 # Application definition
